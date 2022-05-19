26bbe013a779c41a72b74815d6b227ba98e6f44b
GK <guitarkid35@gmail.com>
2022-05-19 21:09:52 +0800

    ＝＝

---
+++ _nuxt/static/1638972914/TermsOfUse/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638972914",layout:"default",error:a,serverRendered:true,routePath:"\u002FTermsOfUse",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1b3cc166"}}}}(null));
---
+++ _nuxt/static/1642063273/TermsOfUse/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642063273",layout:"default",error:a,serverRendered:true,routePath:"\u002FTermsOfUse",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5cf8a2b5"}}}}(null));
