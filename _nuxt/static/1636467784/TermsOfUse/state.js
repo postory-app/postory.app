5aa04d6b521c1f95b2091d213cd226a4df8ca7df
GK <guitarkid35@gmail.com>
2022-05-19 20:59:48 +0800

    持續手動新增.

--- _nuxt/static/1636467784/TermsOfUse/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1636467784",layout:"default",error:a,serverRendered:true,routePath:"\u002FTermsOfUse",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"70e78b6e"}}}}(null));
