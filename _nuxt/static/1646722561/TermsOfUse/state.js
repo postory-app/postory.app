62c9c97292bfe813b82ed50e92849c3e766206d5
GK <guitarkid35@gmail.com>
2022-05-19 21:09:52 +0800

    chore(ci): update assets

---
+++ _nuxt/static/1646722561/TermsOfUse/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646722561",layout:"default",error:a,serverRendered:true,routePath:"\u002FTermsOfUse",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8669e049"}}}}(null));
