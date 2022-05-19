9593154aecf7ddabe8087331d304a0cd2e7c260f
GK <guitarkid35@gmail.com>
2022-05-19 21:32:44 +0800

    test

--- _nuxt/static/1631027085/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631027085",layout:"empty",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"02358fea"}}}}(null,"\u002F"));
--- _nuxt/static/1652259270/TermsOfUse/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/TermsOfUse", {data:[{}],fetch:{},mutations:void 0});
